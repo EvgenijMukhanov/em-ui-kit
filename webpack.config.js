045f3645b35489dd54bdf598ac41f9d04902eba9
Evgenij Mukhanov <evgenij.mukhanov@gmail.com>
2023-02-24 16:23:24 +0300

    em-ui-kit fix settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-import { resolve as _resolve } from 'path';
+const path = require('path');
 
 
-export const mode = 'production';
-export const entry = './src/index.ts';
-export const output = {
-    filename: "index.js",
-    path: _resolve(__dirname, 'dist'),
-    libraryTarget: "umd",
-    clean: true
-};
-export const resolve = {
-    extensions: ['.ts', '.tsx']
-};
-export const externals = {
-    react: 'react'
-};
-export const module = {
-    rules: [
-        {
-            test: /\.css/,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(ts|tsx)?$/,
-            use: ['ts-loader'],
-            exclude: /node_modules/
-        }
-    ],
-};
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "umd",
+        clean: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx']
+    },
+    externals: {
+        react: 'react'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(ts|tsx)?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
+        ],
+    }
+}
