68a7226f61c0d283e743d96cd3aef95335eaa289
Evgenij Mukhanov <evgenij.mukhanov@gmail.com>
2023-02-25 20:48:43 +0300

    em-ui-kit fix

--- src/native/Button/Button.tsx
+++ src/native/Button/Button.tsx
@@ -1,15 +1,22 @@
-import React, { FC } from "react";
+import React, { FC, DetailedHTMLProps, ButtonHTMLAttributes, AriaAttributes } from "react";
 
-import './Button.css'
+import './Button.module.css'
+// import classes from './Button.module.css'
 import '../../index.css'
 
-export interface IButton {
-  children?: React.ReactNode
-}
+export interface IButton extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+  AriaAttributes{
+    children?: React.ReactNode
+  }
 
 export const Button:FC<IButton> = ({ children, ...props }) => {
 
+  // const rootClasses = [classes.button]
   const rootClasses = ['button']
+
+  if (props.className) {
+    rootClasses.push(props.className)
+  }
 
   return (
     <button {...props} className={rootClasses.join(' ')}>{ children }</button>
