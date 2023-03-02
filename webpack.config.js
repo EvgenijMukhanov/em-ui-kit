d1f4086c511c872aba97a20ab0fd11b589267523
Evgenij Mukhanov <evgenij.mukhanov@gmail.com>
2023-03-02 22:11:30 +0300

    resetting

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,41 @@
 const path = require('path');
 
-
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: "umd",
-        clean: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx']
-    },
-    externals: {
-        react: 'react'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)?$/,
-                use: ['ts-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader'],
-            }
-            // {
-            //     test: /\.module\.css$/,
-            //     use: [
-            //         {
-            //             loader: "style-loader"
-            //         },
-            //         {
-            //             loader: "css-loader",
-            //             options: {
-            //                 modules: {
-            //                     localIndentName: '[local]__[sha1:hash:hex:7]'
-            //                 }
-            //             }
-            //         }
-            //     ],
-            // },
-            // {
-            //     test: /^((?!\.module).)*css$/,
-            //     use: [
-            //         {
-            //             loader: "style-loader"
-            //         },
-            //         {
-            //             loader: "css-loader"
-            //         }
-            //     ],
-            // }
-        ],
-    }
-}
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: "umd",
+      clean: true
+  },
+  resolve: {
+      extensions: ['.ts', '.tsx']
+  },
+  externals: {
+      react: 'react'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  }
+};
