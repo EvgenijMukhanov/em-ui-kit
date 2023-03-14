d98ccb4892d64d9094d17b1cf15f9eefef63ed22
Evgenij Mukhanov <evgenij.mukhanov@gmail.com>
2023-03-14 20:58:00 +0300

    Button default tests

--- src/native/Button/Button.test.tsx
+++ src/native/Button/Button.test.tsx
@@ -4,8 +4,14 @@ import { render, screen } from '@testing-library/react'
 import { Button } from './Button'
 
 describe('Button', () => {
-  describe('render', () => {
-    test('defauil', () => {
+  describe('default', () => {
+    test('render', () => {
+      render(<Button className="test">Click me</Button>)
+      const button = screen.getByText('Click me')
+      expect(button).toBeTruthy()
+      
+    }),
+    test('apply props', () => {
       render(<Button className="test">Click me</Button>)
       const button = screen.getByText('Click me')
       expect(button.classList.contains('test')).toBeTruthy()
